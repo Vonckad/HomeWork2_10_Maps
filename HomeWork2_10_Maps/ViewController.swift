35d91dc7810bfdb33e84dc26a9c01df7dc7c8466
Vonckad <Vonkad@yandex.ru>
2022-01-11 22:23:51 +0300

    Initial Commit

---
+++ HomeWork2_10_Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2_10_Maps
+//
+//  Created by Vlad Ralovich on 11.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
